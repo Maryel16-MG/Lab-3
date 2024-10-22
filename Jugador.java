b6eeebc30fb3cee2970e5414572a2be9529f1e5f
Thyfa <thmartinezgo@est.utn.ac.cr>
2024-10-21 19:39:22 -0600

    Se inicia el laboratorio

---
+++ Jugador.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author thyfa
+ */
+ public class Jugador {
+    private int saldo;
+
+    public Jugador(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int obtenerSaldo() {
+        return saldo;
+    }
+
+    public void deducirSaldo(int cantidad) {
+        this.saldo -= cantidad;
+    }
+
+    public void agregarSaldo(int cantidad) {
+        this.saldo += cantidad;
+    }
+}
