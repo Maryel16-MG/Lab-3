ca63548cca134c8ef712267fb0fa6e7dcc3cf5b6
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-10-21 19:56:34 -0600

    Se programa la clase Ruleta
    Se lleva a cabo la programacion de la clase ruleta

--- Ruleta/Ruleta.java
+++ Ruleta/Ruleta.java
@@ -4,10 +4,37 @@
  */
 package Ruleta;
 
+import java.util.Random;
+
 /**
  *
  * @author user
  */
 public class Ruleta {
+     private Simbolo[] simbolos;
+    private Simbolo simboloActual;
+
+    public  Ruleta(Simbolo[] simbolos) {
+        this.simbolos = simbolos;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        try {
+            while (true) {
+                int indice = random.nextInt(simbolos.length);
+                simboloActual = simbolos[indice];
+                System.out.println("Carrete: " + simboloActual.getNombre());
+                Thread.sleep(200); // Pausa para simular el giro
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public Simbolo getSimboloActual() {
+        return simboloActual;
+    }
     
 }
