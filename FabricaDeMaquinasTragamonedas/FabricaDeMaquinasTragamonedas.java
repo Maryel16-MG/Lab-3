a0ee612589a9ea61143ecbfee4210a44afa9522b
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-10-21 20:22:48 -0600

    Se programa Fabrica de tragamonedas
    Se lleva a cabo la programacion de la clase FabricaDeTragaMonedas

--- FabricaDeMaquinasTragamonedas/FabricaDeMaquinasTragamonedas.java
+++ FabricaDeMaquinasTragamonedas/FabricaDeMaquinasTragamonedas.java
@@ -4,10 +4,26 @@
  */
 package FabricaDeMaquinasTragamonedas;
 
+import Jugador.Jugador;
+import Ruleta.Ruleta;
+import Simbolo.Simbolo;
+
 /**
  *
  * @author user
  */
 public class FabricaDeMaquinasTragamonedas {
-    
-}
+     public static MaquinaTragamonedas crearMaquinaTragamonedas(String tipo, Jugador jugador) {
+        if (tipo.equals("clásica")) {
+            Ruleta[] ruletas = { new Ruleta(crearSimbolosClasicos()), new Ruleta(crearSimbolosClasicos()), new Ruleta(crearSimbolosClasicos()) };
+            return new MaquinaTragamonedas(ruletas, jugador);
+        }
+        // Puedes crear diferentes tipos con más ruletas o temáticas.
+        return null;
+    }
+
+    private static Simbolo[] crearSimbolosClasicos() {
+        return new Simbolo[]{ new Simbolo("Cereza"), new Simbolo("Limón"), new Simbolo("Siete") };
+    }
+    }
+
